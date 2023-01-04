2b69764defd08c88b35a03421a9191c85ca6d650
Rajasree Laha <116679804+Rajasree460@users.noreply.github.com>
2023-01-05 00:48:33 +0530

    Create bulls_cows.cpp

---
+++ bulls_cows.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    string getHint(string secret, string guess) {
+        int n=secret.size();
+        vector<int>s(10,0);
+        vector<int>g(10,0);
+        int bulls=0,cows=0;
+        for(int i=0;i<n;i++){
+            if(guess[i]==secret[i]) bulls++;
+            else {
+                ++s[secret[i]-'0'];
+                ++g[guess[i]-'0'];
+            }
+        }
+        for(int i=0;i<10;i++){
+            cows+=min(s[i],g[i]);
+        }
+        return to_string(bulls)+'A'+to_string(cows)+'B';
+
+        //tc=O(n)
+        //sc=O(n)
+    }
+};
