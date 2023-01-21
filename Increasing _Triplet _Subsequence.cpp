a6064824053035b94aba2d0be1a941e38f2a0052
Rajasree Laha <116679804+Rajasree460@users.noreply.github.com>
2023-01-21 18:03:19 +0530

    Create Increasing _Triplet _Subsequence.cpp

---
+++ Increasing _Triplet _Subsequence.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool increasingTriplet(vector<int>& nums) {
+        int n = nums.size();
+        if(n < 3) return false;
+
+        int c1 = INT_MAX; 
+        int c2 = INT_MAX; 
+
+        for(auto& i : nums){
+            if(i <= c1) c1 = i; 
+            else if(i <= c2) c2 = i; 
+            else return true;
+        }
+
+        return false;
+    }
+};
